665810d65fe7bf05c783dfd3cb33e265ac1b7c5c
陈哲凡 <31501293@stu.zucc.edu.cn>
2019-05-17 02:11:17 +0800

    [:construction:] 分页 & 删除

--- solutions/serializers.py
+++ solutions/serializers.py
@@ -2,9 +2,18 @@ from rest_framework import serializers
 from solutions.models import Solution
 
 
-class SolutionSerializer(serializers.ModelSerializer):
+class SolutionDetailSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
 
     class Meta:
         model = Solution
-        fields = "__all__"
+        fields = ('id', 'created', 'title', 'oj', 'pid', 'owner', 'content')
+
+
+class SolutionListSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source="owner.username")
+    created = serializers.DateTimeField(format="%Y年%m月%d日 %H:%M", required=False, read_only=True)
+
+    class Meta:
+        model = Solution
+        fields = ('id', 'created', 'title', 'oj', 'pid', 'owner')
--- solutions/views.py
+++ solutions/views.py
@@ -1,12 +1,41 @@
 from rest_framework import permissions, generics
+from rest_framework.pagination import PageNumberPagination
 from taki.permissions import IsOwnerOrReadOnly
 from solutions.models import Solution
-from solutions.serializers import SolutionSerializer
+from solutions.serializers import SolutionListSerializer, SolutionDetailSerializer
+from django_filters import rest_framework as filters
+from rest_framework.filters import OrderingFilter
+
+
+class SolutionResultsSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 10000
+
+
+class SolutionFilter(filters.FilterSet):
+    title = filters.CharFilter(field_name="title", lookup_expr='contains')
+    oj = filters.CharFilter(field_name="oj", lookup_expr='contains')
+    pid = filters.CharFilter(field_name="pid", lookup_expr='contains')
+    owner = filters.CharFilter(field_name='owner__username', lookup_expr='contains')
+
+    class Meta:
+        model = Solution
+        fields = ['title', 'oj', 'pid', 'owner']
 
 
 class SolutionList(generics.ListCreateAPIView):
     queryset = Solution.objects.all()
-    serializer_class = SolutionSerializer
+    # serializer_class = SolutionListSerializer
+    pagination_class = SolutionResultsSetPagination
+    filter_backends = (filters.DjangoFilterBackend, OrderingFilter)
+    filterset_class = SolutionFilter
+    ordering_fields = ('title', 'oj', 'pid', 'owner', 'created')
+
+    def get_serializer_class(self):
+        if self.request.method in permissions.SAFE_METHODS:
+            return SolutionListSerializer
+        return SolutionDetailSerializer
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -14,6 +43,6 @@ class SolutionList(generics.ListCreateAPIView):
 
 class SolutionDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Solution.objects.all()
-    serializer_class = SolutionSerializer
+    serializer_class = SolutionDetailSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
                           IsOwnerOrReadOnly)
