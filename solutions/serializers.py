137fe2af64f3a92641f9fb3f209171b7391c3962
陈哲凡 <31501293@stu.zucc.edu.cn>
2019-08-12 15:56:20 +0800

    [:sparkles:] 题解返回作者真实姓名字段

--- solutions/serializers.py
+++ solutions/serializers.py
@@ -4,16 +4,18 @@ from solutions.models import Solution
 
 class SolutionDetailSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
+    name = serializers.ReadOnlyField(source="owner.profile.name")
 
     class Meta:
         model = Solution
-        fields = ('id', 'created', 'title', 'oj', 'pid', 'owner', 'content')
+        fields = ('id', 'created', 'title', 'oj', 'pid', 'owner', 'content', 'name')
 
 
 class SolutionListSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
+    name = serializers.ReadOnlyField(source="owner.profile.name")
     created = serializers.DateTimeField(format="%Y年%m月%d日 %H:%M", required=False, read_only=True)
 
     class Meta:
         model = Solution
-        fields = ('id', 'created', 'title', 'oj', 'pid', 'owner')
+        fields = ('id', 'created', 'title', 'oj', 'pid', 'owner', 'name')
